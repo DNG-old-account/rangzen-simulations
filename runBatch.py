4579a8d1b24ffaae9da3cf9b77ca50e0346e0725
Adam Lerner <lerner@denovogroup.org>
2014-05-15 21:42:20 -0400

    New runbatch system with flags.txt and numbers.txt and parallel

--- runBatch.py
+++ runBatch.py
@@ -10,9 +10,11 @@ try:
 except:
     exit()
 
-for i in range(trials):
-    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -p > results/"+filebase+"/sybilPopular"+str(i)+".json")
-    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -r > results/"+filebase+"/sybilAverage"+str(i)+".json")
-    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -u > results/"+filebase+"/sybilUnpopular"+str(i)+".json")
-    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -a > results/"+filebase+"/sybilSybil"+str(i)+".json")
+#for i in range(trials):
+#    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -p > results/"+filebase+"/sybilPopular"+str(i)+".json")
+#    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -r > results/"+filebase+"/sybilAverage"+str(i)+".json")
+#    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -u > results/"+filebase+"/sybilUnpopular"+str(i)+".json")
+#    os.system("java -jar -Xms6G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar -a > results/"+filebase+"/sybilSybil"+str(i)+".json")
 
+
+os.system("parallel --max-procs 10 java -jar -Xms4G buck-out/gen/apps/simulation/proximitySimulationNoGUI.jar {1} '>' results/400nodes/sybil{1}{2}.json :::: flags.txt :::: numbers.txt")
